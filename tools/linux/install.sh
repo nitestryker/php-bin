61773e5962514722ad00897e2dee2a3b5598e016
Jeremy Stevens <Jeremiahstevens@gmail.com>
2015-01-26 12:56:04 -0800

    updated installer.sh

--- tools/linux/install.sh
+++ tools/linux/install.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+: '
+   installer.sh 
+
+   @package PHP-Bin
+   @author Jeremy Stevens
+   @copyright 2014-2015 Jeremy Stevens
+   @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+  
+   @description  this chages the file permission moves it to /usr/local/bin
+    you do not need to use the python extension just type phpbin -args
+
+   ex. phpbin -i mylog.log -n mylog -s text
+'
 chmod +x "$PWD/"phpbin.py
 sudo cp phpbin.py /usr/local/bin
 sudo mv /usr/local/bin/phpbin.py /usr/local/bin/phpbin
