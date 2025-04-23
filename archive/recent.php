3a83207c58941a9e44d69e48254df63fd63878a3
Jeremy Stevens <jeremiahstevens@gmail.com>
2025-04-22 21:12:33 -0700

    Update recent.php
    modernized

--- archive/recent.php
+++ archive/recent.php
@@ -1,43 +1,50 @@
 <?php
+declare(strict_types=1);
+
 /**
  * Recent posts for archive directory
  *
  * @package PHP-Bin
  * @author Jeremy Stevens
- * @copyright 2014-2023 Jeremy Stevens
  * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
- * @version 2.0.0
+ * @version 2.0.0-modern
  */
 
+// Disable error output in production (log it instead in real environments)
 error_reporting(0);
-include('../include/config.php'); 
-include('../include/db.php');
-include('../include/session.php');
+
+// Load dependencies
+require_once '../include/config.php';
+require_once '../include/db.php';
+require_once '../include/session.php';
 require_once '../classes/conn.class.php';
 require_once '../classes/archive.class.php';
 
-// Initialize connection and archive class
-$conn = new Conn($mysqli, $config);
-$archive = new Archive($conn->db, $config);
+try {
+    // Initialize database connection and archive handler
+    $conn = new Conn($mysqli, $config);
+    $archive = new Archive($conn->db, $config);
 
-// Get recent posts
-$recentPosts = $archive->getRecentPosts(10);
-echo "<table class=\"table table-striped table-bordered table-condensed\">";
+    // Fetch recent posts
+    $recentPosts = $archive->getRecentPosts(10);
 
-foreach ($recentPosts as $post) {
-    $pasteId = $post['post_id']; 
-    $title = $post['post_title']; 
+    echo '<table class="table table-striped table-bordered table-condensed">';
 
-    if (empty($title)) {
-        $title = "Untitled Paste"; 
+    foreach ($recentPosts as $post) {
+        $pasteId = htmlspecialchars($post['post_id'], ENT_QUOTES, 'UTF-8');
+        $title = htmlspecialchars($post['post_title'] ?: 'Untitled Paste', ENT_QUOTES, 'UTF-8');
+
+        echo <<<HTML
+<tr>
+    <td><a href="../post.php?id={$pasteId}">{$title}</a></td>
+</tr>
+HTML;
     }
 
-    $title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
+    echo '</table>';
 
-    echo '<tr>
-            <td><a href="../post.php?id='.$pasteId.'">'.$title.'</a></td>
-          </tr>';
+} catch (Throwable $e) {
+    // In production, log this instead
+    error_log('Error loading recent posts: ' . $e->getMessage());
+    echo '<p>Unable to load recent posts at this time.</p>';
 }
-echo "</table>";
-?>
